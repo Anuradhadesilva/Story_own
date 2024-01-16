f1c7688c6314e1e8e7c6211f5411e742dfa24c05
Anuradhadesilva <anuradhashashimal2@gmail.com>
2024-01-16 13:25:15 +0530

    Initial Commit

---
+++ Story_own/Story_ownApp.swift
@@ -0,0 +1,17 @@
+//
+//  Story_ownApp.swift
+//  Story_own
+//
+//  Created by Anuradha Desilva on 16/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Story_ownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
