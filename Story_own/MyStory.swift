db8bf37a22e801a01ac53faf8d49bc7a54cd1d56
Anuradhadesilva <anuradhashashimal2@gmail.com>
2024-01-16 13:32:27 +0530

    add story data

---
+++ Story_own/MyStory.swift
@@ -0,0 +1,18 @@
+//
+//  MyStory.swift
+//  Story_own
+//
+//  Created by Anuradha Desilva on 16/01/2024.
+//
+
+import SwiftUI
+
+struct MyStory: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyStory()
+}
